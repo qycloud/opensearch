92728e49b661d8a33097421aa8b4c0a7d5157f3a
zhangchaojian-anyuan <983675816@qq.com>
2019-08-09 14:10:46 +0800

    Fix `each()` function has been DEPRECATED as of PHP 7.2.0.

--- CloudsearchClient.php
+++ CloudsearchClient.php
@@ -311,8 +311,8 @@ class CloudsearchClient {
     $query = '';
     $arg = '';
     if(is_array($params) && !empty($params)){
-      while (list ($key, $val) = each ($params)) {
-        $arg .= $this->_percentEncode($key) . "=" . $this->_percentEncode($val) . "&";
+      foreach ($params as $key => $val) {
+          $arg .= $this->_percentEncode($key) . "=" . $this->_percentEncode($val) . "&";
       }
       $query = substr($arg, 0, count($arg) - 2);
     }
@@ -329,12 +329,12 @@ class CloudsearchClient {
    */
   protected function _params_filter($parameters = array()){
     $params = array();
-    while (list ($key, $val) = each ($parameters)) {
-      if ($key == "Signature" ||$val === "" || $val === NULL){
-        continue;
-      } else {
-        $params[$key] = $parameters[$key];
-      }
+    foreach ($parameters as $key => $val) {
+        if ($key == "Signature" || $val === "" || $val === NULL) {
+            continue;
+        } else {
+            $params[$key] = $parameters[$key];
+        }
     }
     ksort($params);
     reset($params);
